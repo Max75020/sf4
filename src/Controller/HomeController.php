9910be4cbeb33750696c55dac29afd05cdd0f21e
Max75020 <maxime.duplaissy@gmail.com>
2019-12-12 10:59:59 +0100

    ajout du top 100

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 
 use App\Entity\Artist;
+use App\Repository\RecordRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,9 +16,13 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(RecordRepository $recordRepository)
     {
+        // Récupération du top 100
+        $top = $recordRepository->getBestRatedOfYear();
 
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', [
+            'top' => $top
+        ]);
     }
 }
